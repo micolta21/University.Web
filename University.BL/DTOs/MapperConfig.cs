1dd31586e9efa6a865dda928903e3ae18baea0c9
Javier <Javier@DESKTOP-B6ROK0N>
2022-03-29 20:04:41 -0500

    Clase #3 8:00

---
+++ University.BL/DTOs/MapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using University.BL.Models;
+using AutoMapper;
+
+namespace University.BL.DTOs
+{
+    public class MapperConfig
+    {
+        public static MapperConfiguration MapperConfiguration()
+        {
+            return new MapperConfiguration(x => {
+
+                x.CreateMap<Course, CourseDTO>().ReverseMap();
+                x.CreateMap<Student, StudentDTO>().ReverseMap();
+                x.CreateMap<Instructor, InstructorDTO>().ReverseMap();
+                x.CreateMap<Student, StudentDTO>().ReverseMap();
+                x.CreateMap<OfficeAssignment, OfficeAssignmentDTO>().ReverseMap();
+
+            });
+        }
+    }
+}
