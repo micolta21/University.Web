35230a5a954d28164b00c88e0f0d922434cf5988
Javier <Javier@DESKTOP-B6ROK0N>
2022-03-31 21:29:55 -0500

    Clase #4 Funcionanado todo

---
+++ University.BL/DTOs/ResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.BL.DTOs
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; }
+        public object Data { get; set; }
+        public string Message { get; set; }
+    }
+}
